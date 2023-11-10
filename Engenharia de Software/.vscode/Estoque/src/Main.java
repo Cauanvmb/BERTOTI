39f94f0bf1c234a913098c8f51f1289d6d5dc459
Cauan Barbaglio <127343662+Cauanvmb@users.noreply.github.com>
2023-11-10 20:43:57 -0300

    atualizado

--- Engenharia de Software/.vscode/Estoque/src/Main.java
+++ Engenharia de Software/.vscode/Estoque/src/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
         
         Estoque criarestoque = new Estoque();
-        criarestoque.EstoqueEmUso(); 
+        criarestoque.estoqueEmUso(); 
     }
 }
